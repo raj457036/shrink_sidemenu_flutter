c9926da523dea7dac2946f24720e0e0d5876ce74
avdisx <avs.sezer@hotmail.com>
2022-09-05 14:51:04 +0300

    Supported background gradient color

--- lib/src/shrink_slide_rotate_menu.dart
+++ lib/src/shrink_slide_rotate_menu.dart
@@ -9,32 +9,31 @@ class ShrinkSlideRotateSideMenuState extends SideMenuState {
 
     return Material(
       color: widget.background ?? const Color(0xFF112473),
-      child: Stack(
-        fit: StackFit.expand,
-        children: [
-          Positioned(
-            top: statusBarHeight + (widget?.closeIcon?.size ?? 25.0) * 2,
-            bottom: 0.0,
-            width: min(size.width * 0.70, widget.maxMenuWidth),
-            right: widget._inverse == 1 ? null : 0,
-            child: widget.menu,
-          ),
-          _getCloseButton(statusBarHeight),
-          AnimatedContainer(
-            duration: const Duration(milliseconds: 350),
-            curve: Curves.fastLinearToSlowEaseIn,
-            transform: _getMatrix4(size),
-            decoration: BoxDecoration(
-                borderRadius: _getBorderRadius(),
-                boxShadow: [
-                  BoxShadow(
-                      offset: Offset(0, 18.0),
-                      color: Colors.black12,
-                      blurRadius: 32.0)
-                ]),
-            child: _getChild(),
-          ),
-        ],
+      child: Container(
+        decoration: BoxDecoration(
+          color: widget.background ?? const Color(0xFF112473),
+          gradient: widget.backgroundGradient,
+        ),
+        child: Stack(
+          fit: StackFit.expand,
+          children: [
+            Positioned(
+              top: statusBarHeight + (widget?.closeIcon?.size ?? 25.0) * 2,
+              bottom: 0.0,
+              width: min(size.width * 0.70, widget.maxMenuWidth),
+              right: widget._inverse == 1 ? null : 0,
+              child: widget.menu,
+            ),
+            _getCloseButton(statusBarHeight),
+            AnimatedContainer(
+              duration: const Duration(milliseconds: 350),
+              curve: Curves.fastLinearToSlowEaseIn,
+              transform: _getMatrix4(size),
+              decoration: BoxDecoration(borderRadius: _getBorderRadius(), boxShadow: [BoxShadow(offset: Offset(0, 18.0), color: Colors.black12, blurRadius: 32.0)]),
+              child: _getChild(),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -49,20 +48,14 @@ class ShrinkSlideRotateSideMenuState extends SideMenuState {
         )
       : widget.child;
 
-  BorderRadius _getBorderRadius() => _opened
-      ? (widget.radius ?? BorderRadius.circular(34.0))
-      : BorderRadius.zero;
+  BorderRadius _getBorderRadius() => _opened ? (widget.radius ?? BorderRadius.circular(34.0)) : BorderRadius.zero;
 
   Matrix4 _getMatrix4(Size size) {
     if (_opened) {
       return Matrix4.identity()
         ..rotateZ(widget.degToRad(5.0 * widget._inverse))
         ..invertRotation()
-        ..translate(
-            min(size.width, widget.maxMenuWidth) *
-                widget._inverse *
-                (widget.inverse ? 0.6 : 0.9),
-            (size.height * 0.1))
+        ..translate(min(size.width, widget.maxMenuWidth) * widget._inverse * (widget.inverse ? 0.6 : 0.9), (size.height * 0.1))
         ..scale(widget.maxMenuWidth / size.width ?? 0.8, 0.8);
     }
     return Matrix4.identity();
