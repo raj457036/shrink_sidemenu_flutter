c898961bace49f0bcb18c362ad881bb9d14d6525
avdisx <avs.sezer@hotmail.com>
2022-09-05 14:56:38 +0300

    Supported background gradient color

---
+++ example/aa.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+Widget aa() {
+  return Container(
+    decoration: BoxDecoration(
+      gradient: LinearGradient(
+        colors: [Colors.blue, Colors.blue[700]],
+      ),
+    ),
+  );
+}
